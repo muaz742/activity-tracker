0fe657e136fd423363a1402db8a0e8c76aaefa01
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2022-07-15 17:17:50 +0300

    changes

--- activity-tracker/App.js
+++ activity-tracker/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Muaz to rEACT nativeDSFA21</Text>
+    
+    <Home />
      
     </View>
   );
